8db5f5b5c89c76e76977b6686ee53e357647a4e8
iraz ramin <irazramin@gmail.com>
2022-12-28 16:58:29 +0600

    resume template added

--- pages/portal/user/resume/index.tsx
+++ pages/portal/user/resume/index.tsx
@@ -8,7 +8,232 @@ const Resume = () => {
                 <TitleCard title="Resume">
                 </TitleCard>
                 <DefaultCard>
+                    <section className="template">
+                        <div className="template-header-bg">
+                            <div className="name-positions">
+                                <h1 className="person-name">Iraz Ramin</h1>
+                                <p>Fullstack Developer</p>
+                            </div>
+                        </div>
+                        <div className="basic-details">
+                            <img
+                                src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                                alt=""
+                                className="person-img"
+                            />
+                            <div className="about">
+                                <div className="addresses">
+                                    <div className="address">
+                                        <i className="bx bx-location-plus icon"></i>
+                                        <span>Dhaka, Bangladesh</span>
+                                    </div>
+                                    <div className="address">
+                                        <i className="bx bx-phone icon"></i>
+                                        <span>0192283837</span>
+                                    </div>
+                                    <div className="address">
+                                        <i className="bx bx-envelope icon"></i>
+                                        <span>irazramin@gmail.com</span>
+                                    </div>
+                                    <div className="address">
+                                        <i className="bx bx-world icon"></i>
+                                        <span>www.irazramin.me</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div className="resume-body">
+                            <div className="career-objective">
+                                <h4>Career Objective</h4>
+                                <p>
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis
+                                    rem, sit sapiente distinctio asperiores eum maxime in eveniet
+                                    iure. Sapiente ratione nam velit pariatur nisi corporis cum ut
+                                    possimus, ullam facere suscipit commodi doloremque voluptas.
+                                    Temporibus sed perferendis commodi delectus esse amet unde
+                                    assumenda? Delectus corrupti at culpa laborum fugit.
+                                </p>
+                            </div>
+
+                            <div className="skills-section">
+                                <h4>Skills</h4>
+                                <div className="skills">
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                    <div className="skill">
+                                        <div className="skill-details">
+                                            <span className="skill-name">Python</span>
+                                            <span className="skill-percentage">70%</span>
+                                        </div>
+                                        <div className="progress-bar">
+                                            <div className="progress"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="job-experience">
+                                <h4>Job Experiences</h4>
+                                <ul className="experiences">
+                                    <li>
+                                        <h5>
+                                            <span className="position">Enter Job Position</span>
+                                            <span className="years">2014-2016</span>
+                                        </h5>
+                                        <h6>
+                    <span className="company-name">Company Name</span
+                    ><span className="city">New York City</span>
+                                        </h6>
+                                        <p className="description">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Repellat tenetur culpa eius unde ipsam error porro dolorem
+                                            sit aliquid sint!
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <h5>
+                                            <span className="position">Enter Job Position</span>
+                                            <span className="years">2014-2016</span>
+                                        </h5>
+                                        <h6>
+                    <span className="company-name">Company Name</span
+                    ><span className="city">New York City</span>
+                                        </h6>
+                                        <p className="description">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Repellat tenetur culpa eius unde ipsam error porro dolorem
+                                            sit aliquid sint!
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <h5>
+                                            <span className="position">Enter Job Position</span>
+                                            <span className="years">2014-2016</span>
+                                        </h5>
+                                        <h6>
+                    <span className="company-name">Company Name</span
+                    ><span className="city">New York City</span>
+                                        </h6>
+                                        <p className="description">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Repellat tenetur culpa eius unde ipsam error porro dolorem
+                                            sit aliquid sint!
+                                        </p>
+                                    </li>
+                                    <li>
+                                        <h5>
+                                            <span className="position">Enter Job Position</span>
+                                            <span className="years">2014-2016</span>
+                                        </h5>
+                                        <h6>
+                    <span className="company-name">Company Name</span
+                    ><span className="city">New York City</span>
+                                        </h6>
+                                        <p className="description">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                            Repellat tenetur culpa eius unde ipsam error porro dolorem
+                                            sit aliquid sint!
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="education-section">
+                                <h4>Education</h4>
+                                <ul className="education">
+                                    <li>
+
+                                        <h5>
+                                            <span className="degree">Enter your degree</span>
+                                        </h5>
+                                        <span className="years">2014-2016</span>
+                                        <h6>
+                      <span className="university-name">University Name</span
+                      ><span className="city">New York City</span>
+                                        </h6>
+                                    </li>
+                                    <li>
+
+                                        <h5>
+                                            <span className="degree">Enter your degree</span>
+                                        </h5>
+                                        <span className="years">2014-2016</span>
+                                        <h6>
+                      <span className="university-name">University Name</span
+                      ><span className="city">New York City</span>
+                                        </h6>
+                                    </li>
+                                    <li>
+
+                                        <h5>
+                                            <span className="degree">Enter your degree</span>
+                                        </h5>
+                                        <span className="years">2014-2016</span>
+                                        <h6>
+                      <span className="university-name">University Name</span
+                      ><span className="city">New York City</span>
+                                        </h6>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+
+                        </div>
+                    </section>
                 </DefaultCard>
             </UserPortalLayout>
         </>
